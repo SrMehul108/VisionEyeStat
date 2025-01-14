5e00bc674cf6dab1bab41459815df704d17a8cd1
SrMehul108 <mehul.gupta@swiftrut.com>
2025-01-14 06:32:41 +0530

    Slider Set

--- Js/main.js
+++ Js/main.js
@@ -27,3 +27,40 @@ document.querySelectorAll('.nav-link').forEach(link => {
 
 //  SERVICE SECTION SLIDER
 
+document.addEventListener("DOMContentLoaded", () => {
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        slidesPerView: 1,
+        spaceBetween: 10,
+        breakpoints: {
+            320:{
+                slidesPerView: 1,
+                spaceBetween: 50,
+            },
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 30,
+            },
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
